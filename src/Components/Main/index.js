a2735a425811c4d734da1552a40188ccfdb8d3ab
Malachi <malachialusa@gmail.com>
2021-09-30 13:27:57 -0600

    Added file structure and basic frame

---
+++ src/Components/Main/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+function Main() {
+
+
+
+    return(
+        <div className="MainflexLg">
+
+            <div className="PadLg">
+
+               <div className="PadTitle">
+                   Your Pad
+               </div>
+
+               <button className="CreateButton"> Create New Note +</button>
+               <button className="CreateButton"> Create New Event +</button>
+               <button className="CreateButton"> Create New Calculation +</button>
+
+               <div className="PadTitle">
+                    My Notes
+               </div>
+
+               
+
+
+            </div>
+        </div>
+    );
+};
+export default Main
