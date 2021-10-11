94a1fa07e059805dde98908423c3e32bff4c62fc
Malachi <malachialusa@gmail.com>
2021-10-11 11:36:07 -0600

    added basic layout

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -35,15 +35,80 @@ const [note, setNote] =React.useState([
 
 ]);
 
+const [displayedNote, setDisplayedNote] = React.useState([
+
+    {NoteName: ""},
+    { Text: "" },
+    {DateTime: ""}
+])
+
+
+
+  /*  function displayNote(){
+
+        return(
+
+            <div className="NoteFlex">
+                <div className="NoteInfo">
+                <div className="NoteTitle">{displayedNote.NoteName}</div>
+                <div className="NoteTime">{displayedNote.DateTime}</div>
+                <button className="KillButton">Delete note</button>
+                </div>
+                <div className="NoteText">{displayedNote.Text}</div>
+            </div>
+
+        )
+    } */
+
+    function handleNoteSelection({note}){
+        const ogNote = [displayedNote.NoteName ] ;
+
+        setDisplayedNote(note);
+
+    }
+
     function NoteItem ({note}){
 
         return(
-            <button className="NoteItem">{note.NoteName} </button>  
+            <div>
+            <div className="NoteDate">{note.DateTime}</div>
+            <button onClick={handleNoteSelection}  className="NoteItem">{note.NoteName} </button>  
+            </div>
         )
     }
+    
 
+    const addNote = text => {
+        const newNote = [...note, {text}];
+        setNote(newNote);
+    };
 
+    function NoteInput({ addNote }) {
+        const [value, setValue] = React.useState("");
+        const handleSubmit = e =>{
+            e.preventDefault();
+            if (!value) return;
+            addNote(value);
+            setValue("");
+        };
 
+        return(
+            <form onSubmit={handleSubmit}>
+
+            <textarea className="NoteField" value={value} onChange={e=> setValue(e.target.value)}>
+
+            </textarea>
+            
+            </form>
+        )
+
+        
+    }
+
+    function CreateNewNote (){
+
+  
+    }
 
 
     return(
@@ -55,7 +120,7 @@ const [note, setNote] =React.useState([
                    Your Pad
                </div>
 
-               <button className="CreateButton"> Create New Note +</button>
+               <button className="CreateButton" > Create New Note +</button>
                <button className="CreateButton"> Create New Event +</button>
                <button className="CreateButton"> Create New Calculation +</button>
 
@@ -65,6 +130,7 @@ const [note, setNote] =React.useState([
 
 
                <div className="SavedNotesFlex">
+              
                 {note.map(
                     ((note, index) => (<NoteItem note={note} index= {index} key={index} />))
                 )}
@@ -73,18 +139,25 @@ const [note, setNote] =React.useState([
             </div>
 
             <div className="NoteFlex">
-
-            
-
-
-
-
-
+                <div className="NoteInfo">
+                <div className="NoteTitle">{displayedNote.NoteName}</div>
+                <div className="NoteTime">{displayedNote.DateTime}</div>
+                <button className="KillButton">Delete note</button>
+                </div>
+                <div className="NoteText">{displayedNote.Text}</div>
             </div>
+
+
+            <div className="EventPad">This will be our Events display</div>
 
 
 
         </div>
+
+
+
+
+
     );
 };
 export default Main
